2b8d05129568af5cdeccb085ad991a079e09ad50
Yassir <yassir.solomah@gmail.com>
2022-04-20 02:31:23 -0700

    move to config file for params

---
+++ config.py
@@ -0,0 +1,52 @@
+import board
+import neopixel
+
+# Minimum frequency band to keep around in melmat/triangular filters
+MIN_FREQ = 100
+
+# Maximum frequency band to keep around in the melmat/triangular filters
+MAX_FREQ = 15000
+
+# Number of samples to read in single iteration from mic
+CHUNK = 4096
+
+# Sampling rate of mic
+RATE = 44100
+
+# Number of pixels on LED strip
+NUM_PIXELS = 60
+
+# Samples to store in moving average for denoising
+# Loop roughly takes 0.15s, and with delay of 0.1s
+# this implies 0.25s * 25 = 6.25s of moving average
+MOVING_AVG_LEN = 25
+
+# Times to run loop, set to -1 for while true
+NUM_TIMES_TO_RUN_LOOP = 1000
+
+# Raspberry PI pin to push data to
+PI_PIN = board.D21
+
+# Brightness multiplier
+BRIGHTNESS_MULTIPLIER = 0.5
+
+# Pixel data order
+PIXEL_DATA_ORDER = neopixel.GRB
+
+# Autowrite LED
+AUTO_WRITE_LED = False
+
+# Relative movement threshold for minimum display
+# This implies the FFT bucketized value after melmat mult must
+# be at least (1 + threshold) relatively greater than the moving
+# average
+THRESHOLD_FOR_LED_DISP = 0.02
+
+# Global LED Pixel Multiplier
+LED_PIXEL_MULTIPLIER = 0.50
+
+# Bias to prevent zero division with LEDs array
+DIVISION_ADDED_BIAS = 0.001
+
+# Added Sleep for LED display in ms
+ADDED_SLEEP_MS = 50
--- mic_test.py
+++ mic_test.py
@@ -10,6 +10,8 @@ from scipy import interpolate
 import board
 import neopixel
 
+import config
+
 
 def colour_wheel(pixel_pos, num_led):
     wheel_pos = (255 / num_led) * pixel_pos
@@ -31,9 +33,6 @@ def colour_wheel(pixel_pos, num_led):
     return (r, g, b)
         
 
-MIN_FREQ = 100
-MAX_FREQ = 15000
-
 p = pyaudio.PyAudio()
 info = p.get_host_api_info_by_index(0)
 num_dev = info.get('deviceCount')
@@ -42,16 +41,7 @@ for i in range(0, num_dev):
         print('Input dev id ', i, ' - ', p.get_device_info_by_host_api_device_index(0, i))
 
 
-CHUNK = 4096
-RATE = 44100
-#stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True,
-#    frames_per_buffer=CHUNK)
-    
-#for i in range(10):
-#    data = np.fromstring(stream.read(CHUNK), dtype=np.int16)
-#    print(data)
-
-melmat, (melfreq, _) = compute_melmat(60, MIN_FREQ, MAX_FREQ, num_fft_bands=22051, sample_rate=44100)
+melmat, (melfreq, _) = compute_melmat(config.NUM_PIXELS, config.MIN_FREQ, config.MAX_FREQ, num_fft_bands=(config.RATE // 2) + 1, sample_rate=config.RATE)
 sum_melmat = melmat.sum(axis=1)
 norm_melmat = melmat / sum_melmat[:, np.newaxis]
 
@@ -59,72 +49,35 @@ arr_of_vals = []
 moving_avg_arr = None
 
 wheel_pos_arr = []
-for i in range(60):
-    wheel_pos_arr.append(colour_wheel(i, 60))
+for i in range(config.NUM_PIXELS):
+    wheel_pos_arr.append(colour_wheel(i, config.NUM_PIXELS))
 
 print(wheel_pos_arr)
 
-NUM_PIXELS = 60
+pixels = neopixel.NeoPixel(config.PI_PIN, config.NUM_PIXELS, brightness=config.BRIGHTNESS_MULTIPLIER, pixel_order=config.PIXEL_DATA_ORDER, auto_write=config.AUTO_WRITE_LED)
 
-pixels = neopixel.NeoPixel(board.D21, NUM_PIXELS, brightness=0.5, pixel_order=neopixel.GRB, auto_write=False)
-
-MOVING_AVG_LEN = 25
-
-num_times = 1000
 done_loop = 0
-while done_loop < num_times:
+while done_loop < config.NUM_TIMES_TO_RUN_LOOP:
     time_0 = time.time()
     print(done_loop)
     done_loop = done_loop + 1
-    stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True,
-    frames_per_buffer=CHUNK)
-    data = np.fromstring(stream.read(CHUNK), dtype=np.int16)
+    stream = p.open(format=pyaudio.paInt16, channels=1, rate=config.RATE, input=True, frames_per_buffer=config.CHUNK)
+    data = np.fromstring(stream.read(config.CHUNK), dtype=np.int16)
     stream.stop_stream()
     stream.close()
-    fft_arr = np.abs(fft(data)[1:CHUNK//2])
-    #print(np.shape(fft_arr))
-    freq_buckets = fftfreq(CHUNK, d=1/RATE)[1:CHUNK//2]
-    #print(len(freq_buckets))
-    #print(len(fft_arr))
-    #print(np.shape(melmat))
-    full_freq = np.zeros(RATE//2 + 1)
+    fft_arr = np.abs(fft(data)[1:config.CHUNK // 2])
+    freq_buckets = fftfreq(config.CHUNK, d=1 / config.RATE)[1:config.CHUNK // 2]
+    full_freq = np.zeros((config.RATE // 2) + 1)
     #print(np.shape(full_freq))
     time_1 = time.time()
     
     f_scipy_interpol = interpolate.interp1d(freq_buckets, fft_arr, fill_value="extrapolate")
     
     print('time of stream open and fft and freq bucket : ', time_1 - time_0)
-    #for i in range(len(freq_buckets)):
-    #    #print(freq_buckets[i])
-    #    left_idx = int(freq_buckets[i])
-    #    right_idx = left_idx + 1
-    #    full_freq[left_idx] = fft_arr[i]
-    #    full_freq[right_idx] = fft_arr[i]
-    #    if (i == 0 or i == len(freq_buckets) -1):
-    #        print('left idx ', left_idx)
-    #        print('right idx ', right_idx)
-    time_2 = time.time()
-    print('time of make freq array : ', time_2 - time_1)
-    #prev_value = 0;
-    # ACTUAL LINEAR INTEPOLATE 1d interpol scipy???
-    #for i in range(len(full_freq)):
-    #    if (full_freq[i] > 0):
-    #        prev_value = full_freq[i]
-    #    if (full_freq[i] == 0):
-    #        full_freq[i] = prev_value
     time_3 = time.time()
-    print('time of full freq array : ', time_3 - time_2)
     
-    fft_freqs_new_bucket = np.arange(0, 22051, 1)
+    fft_freqs_new_bucket = np.arange(0, (config.RATE // 2) + 1, 1)
     new_full_freq_to_plot = f_scipy_interpol(fft_freqs_new_bucket)
-    #print(np.ndim(new_full_freq_to_plot))
-    #print(np.shape(new_full_freq_to_plot))
-    #print(new_full_freq_to_plot.dtype)
-    #print(np.ndim(full_freq))
-    #print(np.shape(full_freq))
-    #print(full_freq.dtype)
-    
-    #full_freq_to_plot = np.log(np.dot(melmat, full_freq))
 
     full_freq_to_plot = np.log(melmat.dot(new_full_freq_to_plot))
     
@@ -136,50 +89,33 @@ while done_loop < num_times:
     
     #print(np.shape(full_freq_to_plot))
     
-    if (len(arr_of_vals) >= MOVING_AVG_LEN):
+    if (len(arr_of_vals) >= config.MOVING_AVG_LEN):
         remove_from_avg = arr_of_vals.pop()
         arr_of_vals.append(full_freq_to_plot)
-        moving_avg_arr = moving_avg_arr - (remove_from_avg / MOVING_AVG_LEN) + (full_freq_to_plot / MOVING_AVG_LEN)
+        moving_avg_arr = moving_avg_arr - (remove_from_avg / config.MOVING_AVG_LEN) + (full_freq_to_plot / config.MOVING_AVG_LEN)
         #print(moving_avg_arr)
     else:
         arr_of_vals.append(full_freq_to_plot)
         if (moving_avg_arr is None):
-            moving_avg_arr = (full_freq_to_plot / MOVING_AVG_LEN)
+            moving_avg_arr = (full_freq_to_plot / config.MOVING_AVG_LEN)
         else:
-            moving_avg_arr = moving_avg_arr + (full_freq_to_plot / MOVING_AVG_LEN)
+            moving_avg_arr = moving_avg_arr + (full_freq_to_plot / config.MOVING_AVG_LEN)
             
     time_5 = time.time()
     print('time of moving avg : ', time_5 - time_4)
 
     
-    #print(np.shape(full_freq_to_plot))
-    #print(len(arr_of_vals))
-    
-    #if (done_loop % 2 == 0):
-    #    plt.plot(freq_buckets[100:1500], np.log(fft_arr[100:1500]))
-    #else:
-    #    if (len(arr_of_vals) >= 10):
-    #        plot_not_normalized = np.divide((full_freq_to_plot - moving_avg_arr), moving_avg_arr)
-    #        plot_not_normalized = np.maximum(plot_not_normalized, 0)
-    #        plt.plot(plot_not_normalized / np.amax(plot_not_normalized + 0.0001))
-    #        #plt.plot(full_freq_to_plot - moving_avg_arr)
-    #        print(full_freq_to_plot - moving_avg_arr)
-    #    else:
-    #        plt.plot(np.log(full_freq_to_plot))
         
          
     leds_to_disp = np.divide((full_freq_to_plot - moving_avg_arr), moving_avg_arr)
     leds_to_disp = np.maximum(leds_to_disp, 0)
-    print(leds_to_disp < 0.02)
-    leds_to_disp = np.floor(leds_to_disp, where=leds_to_disp < 0.02)
-    #print("beforebefore loop ", len(leds_to_disp))
-    #print("before loop ", len(leds_to_disp))
+    leds_to_disp = np.floor(leds_to_disp, where=leds_to_disp < config.THRESHOLD_FOR_LED_DISP)
     if (len(leds_to_disp) == 0):
         pixels.fill((0, 0, 0))
     else:
         #print(leds_to_disp)
         #print(len(leds_to_disp))
-        leds_to_disp = leds_to_disp / np.amax(leds_to_disp + 0.0001)
+        leds_to_disp = leds_to_disp / np.amax(leds_to_disp + config.DIVISION_ADDED_BIAS)
         #print(leds_to_disp)
         pixels.fill((0, 0, 0))
         for i in range(0, 60):
@@ -187,7 +123,7 @@ while done_loop < num_times:
             scalar_factor = leds_to_disp[i]
             
             (r, g, b) = wheel_pos_arr[i]
-            pixels[i] = ((int)(0.50 * scalar_factor * g), (int)(0.50 * scalar_factor * r), (int)(0.50 * scalar_factor * b))
+            pixels[i] = ((int)(config.LED_PIXEL_MULTIPLIER * scalar_factor * g), (int)(config.LED_PIXEL_MULTIPLIER * scalar_factor * r), (int)(config.LED_PIXEL_MULTIPLIER * scalar_factor * b))
     pixels.show()
     
     time_6 = time.time()
@@ -196,12 +132,8 @@ while done_loop < num_times:
 
     print('e2e time : ', time_6 - time_0)
     
-    #plt.grid()
-    #plt.draw()
-    #plt.pause(0.00001)
-    #plt.clf()
     
-    time.sleep(50 / 1000)
+    time.sleep(config.ADDED_SLEEP_MS / 1000)
     
 
 
